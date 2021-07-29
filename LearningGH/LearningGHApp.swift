7ecb831bf8296cad2bfe9a91a61ac35c2f27d022
Israel <indiotizado@gmail.com>
2021-07-29 10:53:16 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Israel Hernandez on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
